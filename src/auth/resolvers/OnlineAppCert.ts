0fc7fcbbe86f8319ade7700897e5cbbca8031e40
Kevin PERROCHES <kevin.perroches@axiome-solution.fr>
2019-02-28 10:03:10 +0100

    Update code for compil

--- src/auth/resolvers/OnlineAppCert.ts
+++ src/auth/resolvers/OnlineAppCert.ts
@@ -1,19 +1,11 @@
 import * as Promise from 'bluebird';
-import { request } from './../../config';
-import { IncomingMessage } from 'http';
-import * as url from 'url';
 
 import { IOnlineAppCert } from './../IAuthOptions';
 import { IAuthResponse } from './../IAuthResponse';
-import { Cache } from './../../utils/Cache';
-import { UrlHelper } from './../../utils/UrlHelper';
-import * as consts from './../../Consts';
 import { OnlineResolver } from './base/OnlineResolver';
 import { HostingEnvironment } from '../HostingEnvironment';
 
 export class OnlineAppCert extends OnlineResolver {
-
-  private static TokenCache: Cache = new Cache();
 
   constructor(_siteUrl: string, private _authOptions: IOnlineAppCert) {
     super(_siteUrl);
@@ -30,7 +22,7 @@ export class OnlineAppCert extends OnlineResolver {
       });
     }
   }
-  
+
   protected InitEndpointsMappings(): void {
     this.endpointsMappings.set(HostingEnvironment.Production, 'accounts.accesscontrol.windows.net');
     this.endpointsMappings.set(HostingEnvironment.China, 'accounts.accesscontrol.chinacloudapi.cn');
